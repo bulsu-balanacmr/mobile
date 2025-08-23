d7349b33d984cbee418345098750485b0339c50e
CloudMonkeyCoding <ejquancey@gmail.com>
2025-08-23 15:33:56 +0800

    Fixed the sql

--- Database/cindys_bakeshop.sql
+++ Database/cindys_bakeshop.sql
@@ -434,11 +434,6 @@ CREATE TABLE `product_ratings` (
   PRIMARY KEY (`Rating_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Sample data for table `product_ratings`
-INSERT INTO `product_ratings` (`Product_Name`, `Average_Rating`, `Total_Review`, `Comments`) VALUES
-('EGG PIE CARAMEL', 4.2, 15, 'Yummy'),
-('PASTEL DELIGHT ROUND CAKE', 3.4, 9, 'wow yummy');
-
 -- Table structure for table `favorites`
 CREATE TABLE `favorites` (
   `Favorite_ID` int(11) NOT NULL AUTO_INCREMENT,
